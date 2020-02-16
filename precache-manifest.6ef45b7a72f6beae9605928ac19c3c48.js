b17e4d917949277cf3dfa71f7a0dd97700eaa82f
yoanad <yoana.dimova@gmail.com>
2020-02-16 15:11:32 +0000

    Updates

---
+++ precache-manifest.6ef45b7a72f6beae9605928ac19c3c48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a949348a2cf7915a1b05cbc6195e4a52",
+    "url": "/datavis-2020/index.html"
+  },
+  {
+    "revision": "3f755d8cb010ab05f773",
+    "url": "/datavis-2020/static/css/main.bb40bf28.chunk.css"
+  },
+  {
+    "revision": "e8bf6a8ab10f850de8eb",
+    "url": "/datavis-2020/static/js/2.ac19b782.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/datavis-2020/static/js/2.ac19b782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f755d8cb010ab05f773",
+    "url": "/datavis-2020/static/js/main.627c6834.chunk.js"
+  },
+  {
+    "revision": "1cd0e713b631b2850be2",
+    "url": "/datavis-2020/static/js/runtime-main.a040696d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/datavis-2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
